437ac0ade7c190fcfd0f7e420205cfc8fbc1ec24
lovekeshpal <lovekeshfgr@gmail.com>
2024-07-27 22:41:39 +0530

    project structuring

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const userRoutes = require("./routes/userRoutes");
+const connectDB = require("./config/db");
+
+/**
+ * This is the main file for the Muscle Mate backend server.
+ * It sets up an Express server and listens on the specified port.
+ * @module index
+ */
+
+require("dotenv").config();
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
+// Middleware
+app.use(bodyParser.json());
 
+// Routes
+app.use("/api/users", userRoutes);
+
+// MongoDB Connection
+connectDB();
+
+/**
+ * Starts the server and listens on the specified port.
+ * @param {number} port - The port number to listen on.
+ */
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
