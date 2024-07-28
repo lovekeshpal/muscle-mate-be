a80a077d246d40dd80fe440166188bb9e0d4bf0c
lovekeshpal <lovekeshfgr@gmail.com>
2024-07-28 16:32:32 +0530

    fixed: Issues with getting the secret key

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const User = require("../models/userModel");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
@@ -5,32 +6,37 @@ const jwt = require("jsonwebtoken");
 const secretKey = process.env.JWT_SECRET;
 
 const signup = async (req, res) => {
-    const { username, password } = req.body;
-    try {
-      const user = new User({ username, password });
-      await user.save();
-      res.status(201).send("User registered successfully");
-    } catch (error) {
-      res.status(400).send("Error registering user");
+  const { username, password } = req.body;
+  try {
+    const user = new User({ username, password });
+    await user.save();
+    res.status(201).send("User registered successfully");
+  } catch (error) {
+    res.status(400).send("Error registering user");
+  }
+};
+
+const login = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      console.error("User not found");
+      return res.status(400).send("User not found");
     }
-  };
-  
-  const login = async (req, res) => {
-    const { username, password } = req.body;
-    try {
-      const user = await User.findOne({ username });
-      if (!user) {
-        return res.status(400).send("User not found");
-      }
-      const isMatch = await bcrypt.compare(password, user.password);
-      if (!isMatch) {
-        return res.status(400).send("Invalid credentials");
-      }
-      const token = jwt.sign({ userId: user._id }, secretKey, { expiresIn: "1h" });
-      res.send({ message: "Login successful", token });
-    } catch (error) {
-      res.status(400).send("Error logging in");
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      console.error("Invalid credentials");
+      return res.status(400).send("Invalid credentials");
     }
-  };
-  
-  module.exports = { signup, login };
+    const token = jwt.sign({ userId: user._id }, secretKey, {
+      expiresIn: "1h",
+    });
+    res.send({ message: "Login successful", token });
+  } catch (error) {
+    console.error("Error logging in:", error);
+    res.status(400).send("Error logging in");
+  }
+};
+
+module.exports = { signup, login };
