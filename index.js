737959a75a86f1d31ccdc3e5000aaf03ebabf851
lovekeshpal <lovekeshfgr@gmail.com>
2024-07-29 23:17:53 +0530

    CORS Policy Setup

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const userRoutes = require("./routes/userRoutes");
 const connectDB = require("./config/db");
 
@@ -14,7 +15,16 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 4000;
 
+// CORS options
+const corsOptions = {
+  origin: "http://localhost:3000", // Allow only this origin
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Allow these HTTP methods
+  credentials: true, // Allow cookies to be sent
+  optionsSuccessStatus: 204, // Some legacy browsers choke on 204
+};
+
 // Middleware
+app.use(cors(corsOptions)); // Enable CORS for all routes
 app.use(bodyParser.json());
 
 // Routes
